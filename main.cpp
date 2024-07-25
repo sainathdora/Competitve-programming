782c03d7cccb6f5d9dfc721f9ff1e5cb2b6268c0
Sainath <sainathdora4@gmail.com>
2024-07-25 08:12:16 +0530

    finished subsets

--- main.cpp
+++ main.cpp
@@ -5,33 +5,19 @@ typedef vector<int> vi;
 typedef vector<string> vs;
 typedef vector<char> vc;
 using namespace std;
-
-int KadaneAlgo(vi arr){
-    int m{0}, s{0};
-    for(int i(0); i<arr.size(); i++){
-        if(i==0){
-            m = arr[i];
-            s = arr[i];
-            continue;
-        }
-        int curr{arr[i]};
-        int prev{arr[i-1]};
-        if((curr%2==0 && prev%2==0) || (curr%2!=0 && prev%2!=0)){
-            s = curr;
-        }else{
-            s += curr;
-            cout<<"s = "<<s<<endl;
-            if(s<0){
-                s = 0;
-            }
-            m = std::max(s, m);
-            cout<<"m = "<<m<<endl;
-            cout<<"curr = "<<curr<<endl;
-            cout<<"----------\n";
-            
-        }
+void find_sub(vc &s, int i, int j, vc &output){
+    // base
+    if(s[i]=='0'){
+        output[j]='0';
+        for(auto &i:output)cout<<i<<" ";
+        cout<<"\n";
+        return;
     }
-    return m;
+    // left, include j
+    output[j] = s[i];
+    find_sub(s, i+1, j+1, output);
+    // right
+    find_sub(s, i+1, j, output);
 }
 
 
@@ -40,16 +26,8 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0),cout.tie(0);
-    int tt;
-    cin>>tt;
-    while (tt--)
-    {
-        int s;
-        cin>>s;
-        vi arr(s);
-        for(int i(0); i<s; ++i)cin>>arr[i];
-        cout<<KadaneAlgo(arr)<<endl;
-    }
-    
+    vc input{'a', 'b', 'c', '0'};
+    vc op(input.size()+1, '0');
+    find_sub(input, 0, 0, op);
     return 0;
 }
