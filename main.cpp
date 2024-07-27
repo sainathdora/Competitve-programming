87b561aeec27fc857b6f603ca455dd7f077c311f
Sainath <sainathdora4@gmail.com>
2024-07-27 15:48:05 +0530

    First Negative Number in every window of size k

--- main.cpp
+++ main.cpp
@@ -6,25 +6,33 @@ typedef vector<string> vs;
 typedef vector<char> vc;
 using namespace std;
 
-int Max_Sub_Array_of_K(vi arr, int k){
+void Negative_Num_of_Every_Window_Size_k(vi arr, int k){
     vi::iterator i = arr.begin();
     vi::iterator j = arr.begin();
-    int sum = 0;
-    int maxi = INT_MIN;
+    vi ans;
+    deque<int>dq;
     while(j!=arr.end()){
         // if j-i +1 == k then find its sum
-        sum += *j;
+        if(*j<0)dq.push_back(*j);
         if(j-i+1<k){
             j++;
         }
         else if(j-i+1==k){
-            maxi = std::max(sum, maxi);
-            sum -= *i;
+            if(dq.size()==0){
+                ans.push_back(0);
+            }
+            else{
+                ans.push_back(dq.front());
+                if(*i == dq.front()){
+                    dq.pop_front();
+                }
+            }
             i++;
             j++;
         }
     }
-    return maxi;
+    for(int &k:ans)cout<<k<<" \n";
+    
     
 }
 
@@ -33,7 +41,7 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0),cout.tie(0);
-    vi arr{1, 9, 3, 4};
-    cout<<Max_Sub_Array_of_K(arr, 2);
+    vi arr{12, -1, -7, 8, -15, 30, 15, 28};
+    Negative_Num_of_Every_Window_Size_k(arr, 3);
     // --sainath dora
 }
