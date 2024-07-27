f1b588e96a6d55d8c08ee0495636b718f8d9dfde
Sainath <sainathdora4@gmail.com>
2024-07-27 10:25:57 +0530

    Largest sub array sum of size k

--- main.cpp
+++ main.cpp
@@ -6,21 +6,25 @@ typedef vector<string> vs;
 typedef vector<char> vc;
 using namespace std;
 
-void permu(string input, string op, vector<string>&vs){
-    if(input.size()==0){
-        vs.push_back(op);
-        return;
-    }
-    // recursive case
-    unordered_set<char>mp;
-    for(int i(0); i<input.size(); ++i){
-        if(mp.find(input[i])==mp.end()){
-            mp.insert(input[i]);
-            string newIP = input.substr(0, i)+input.substr(i+1);
-            string newout = op + input[i];
-            permu(newIP, newout, vs);
+int Max_Sub_Array_of_K(vi arr, int k){
+    vi::iterator i = arr.begin();
+    vi::iterator j = arr.begin();
+    int sum = 0;
+    int maxi = INT_MIN;
+    while(j!=arr.end()){
+        // if j-i +1 == k then find its sum
+        sum += *j;
+        if(j-i+1<k){
+            j++;
+        }
+        else if(j-i+1==k){
+            maxi = std::max(sum, maxi);
+            sum -= *i;
+            i++;
+            j++;
         }
     }
+    return maxi;
     
 }
 
@@ -29,9 +33,7 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0),cout.tie(0);
-    vector<string>vs;
-    permu("abc", "", vs);
-    for(auto i:vs)cout<<i<<endl;
-    return 0;
+    vi arr{1, 9, 3, 4};
+    cout<<Max_Sub_Array_of_K(arr, 2);
     // --sainath dora
 }
