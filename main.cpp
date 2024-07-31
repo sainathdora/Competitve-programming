89286a10eaff19e558a90d8177113adfcfe607bc
Sainath <sainathdora4@gmail.com>
2024-07-31 09:24:04 +0530

    Variable Sliding Window for Largest Subarry of sum k for +ve int

--- main.cpp
+++ main.cpp
@@ -6,52 +6,32 @@ typedef vector<string> vs;
 typedef vector<char> vc;
 using namespace std;
 
-vi find_max_in_subarray(vi arr, int k){
-    vi::iterator i = arr.begin();
-    vi::iterator j = arr.begin();
-    vi ans;
-    deque<int>dq;
-    dq.push_back(INT_MIN);
+int Largest_Subarray_of_Sum_k(vi arr, int k){
+    int maxi = INT_MIN;
+    vi::iterator i = arr.begin(), j = arr.begin();
+    int sum{0};
     while(j!=arr.end()){
-        if(j-i+1<k){
-            if(*j<dq.back())dq.push_back(*j);
-            else{
-                while(dq.size() >0 && dq.back()<*j){
-                    dq.pop_back();
-                }
-                dq.push_back(*j);
+        sum += *j;
+        if(sum>k){
+            while(sum>k){
+                sum -= *i;
+                i++;
             }
-            j++;
-        }else if(j-i+1==k){
-            if(*j<dq.back()){
-                dq.push_back(*j);
-            }
-            else{
-                while(dq.size() >0 && dq.back()<*j){
-                    dq.pop_back();
-                }
-                dq.push_back(*j);
-            }
-            ans.push_back(dq.front());
-            if(dq.front()==*i){
-                dq.pop_front();
-            }
-            i++;
-            j++;
         }
+        else if(sum==k){
+            int size = (j-i+1);
+            maxi = std::max(maxi,size);
+        }
+        j++;
     }
-    return ans;
+    return maxi;
 }
-
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0),cout.tie(0);
-    vi arr{1, 3, -1, -3, 5, 3, 6, 7};
-    // vi arr{3, 1, 2, -1};
-    for(auto &i:find_max_in_subarray(arr, 3)){
-        cout<<i<<endl;
-    }
+    vi arr{4, 1, 1, 1, 2, 3, 5};
+    cout<<Largest_Subarray_of_Sum_k(arr, 5);
     // --sainath dora
 }
