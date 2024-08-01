537d3c5123735470556b238bc648e7fabc817879
Sainath <sainathdora4@gmail.com>
2024-08-01 21:16:19 +0530

    Longest Substring with k unique characters

--- main.cpp
+++ main.cpp
@@ -1,55 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef vector<int> vi;
-#define endl "\n";
+#define endl "\n"
+using ll = long long;
 typedef vector<string> vs;
+typedef vector<long long> vll;
 typedef vector<char> vc;
 using namespace std;
 
-int KadaneAlgo(vi arr){
-    int m{0}, s{0};
-    for(int i(0); i<arr.size(); i++){
-        if(i==0){
-            m = arr[i];
-            s = arr[i];
-            continue;
+int Longest_Substring(string s, int k){
+    string::iterator i = s.begin(), j = s.begin();
+    map<char, int>mp;
+    int maxi = INT_MIN;
+    while(j != s.end()){
+        mp[*j]++;
+        if(mp.size()>k){
+            mp[*i]--;
+            if(mp[*i]==0)mp.erase(*i);
+            i++;
+        }else if(mp.size()==k){
+            int length_of_sub = j-i+1;
+            maxi = std::max(maxi, length_of_sub);
         }
-        int curr{arr[i]};
-        int prev{arr[i-1]};
-        if((curr%2==0 && prev%2==0) || (curr%2!=0 && prev%2!=0)){
-            s = curr;
-        }else{
-            s += curr;
-            cout<<"s = "<<s<<endl;
-            if(s<0){
-                s = 0;
-            }
-            m = std::max(s, m);
-            cout<<"m = "<<m<<endl;
-            cout<<"curr = "<<curr<<endl;
-            cout<<"----------\n";
-            
-        }
+        j++;
     }
-    return m;
+    return maxi;
 }
-
 
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0),cout.tie(0);
-    int tt;
-    cin>>tt;
-    while (tt--)
-    {
-        int s;
-        cin>>s;
-        vi arr(s);
-        for(int i(0); i<s; ++i)cin>>arr[i];
-        cout<<KadaneAlgo(arr)<<endl;
-    }
-    
+    string s = "aabacbebebe";
+    cout<<Longest_Substring(s, 3);
     return 0;
 }
