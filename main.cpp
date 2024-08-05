fea873479d33b4986510a0b0733c5ff6d8e2fec1
Sainath <sainathdora4@gmail.com>
2024-08-05 11:27:17 +0530

    added input.txt output.txt

--- main.cpp
+++ main.cpp
@@ -1,55 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef vector<int> vi;
-#define endl "\n";
+#define endl "\n"
+using ll = long long;
 typedef vector<string> vs;
+typedef vector<long long> vll;
 typedef vector<char> vc;
 using namespace std;
-
-int KadaneAlgo(vi arr){
-    int m{0}, s{0};
-    for(int i(0); i<arr.size(); i++){
-        if(i==0){
-            m = arr[i];
-            s = arr[i];
-            continue;
-        }
-        int curr{arr[i]};
-        int prev{arr[i-1]};
-        if((curr%2==0 && prev%2==0) || (curr%2!=0 && prev%2!=0)){
-            s = curr;
-        }else{
-            s += curr;
-            cout<<"s = "<<s<<endl;
-            if(s<0){
-                s = 0;
-            }
-            m = std::max(s, m);
-            cout<<"m = "<<m<<endl;
-            cout<<"curr = "<<curr<<endl;
-            cout<<"----------\n";
-            
-        }
-    }
-    return m;
-}
-
 
 
 int main()
 {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
     ios::sync_with_stdio(0);
     cin.tie(0),cout.tie(0);
     int tt;
     cin>>tt;
-    while (tt--)
-    {
-        int s;
-        cin>>s;
-        vi arr(s);
-        for(int i(0); i<s; ++i)cin>>arr[i];
-        cout<<KadaneAlgo(arr)<<endl;
+    while(tt--){
+        int n;
+        vi arr(n);
+        
     }
-    
     return 0;
 }
