5aef194fc3448db225f4da5116d5430ea078689d
Sainath <sainathdora4@gmail.com>
2024-08-05 12:23:17 +0530

    Largest Number in K swaps

--- main.cpp
+++ main.cpp
@@ -8,6 +8,33 @@ typedef vector<long long> vll;
 typedef vector<char> vc;
 using namespace std;
 
+void solve(string &s, string &ans, int k, string::iterator start){
+    string::iterator i = start+1;
+    if(k==0 || i == s.end()){
+        return;
+    }
+    char max = *(std::max_element(i, s.end()));
+    bool flag = false;
+    for(; i != s.end(); i++){
+        if(*i==max && *i>*start){
+            flag = true;
+            std::iter_swap(i, start);
+            if(ans<s)ans=s;
+            solve(s, ans, k-1, start+1);
+            std::iter_swap(i, start);
+        }
+    }
+    if(!flag){
+        solve(s, ans, k, start+1);
+    }
+}
+
+string find_largest_Num_k_swaps(string s, int k){
+    string ans = s;
+    string::iterator start  = s.begin();
+    solve(s, ans, k, start);
+    return ans;
+}
 
 int main()
 {
@@ -17,12 +44,6 @@ int main()
     #endif
     ios::sync_with_stdio(0);
     cin.tie(0),cout.tie(0);
-    int tt;
-    cin>>tt;
-    while(tt--){
-        int n;
-        vi arr(n);
-        
-    }
+    cout<<find_largest_Num_k_swaps("721", 5);
     return 0;
 }
